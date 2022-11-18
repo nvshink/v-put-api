147607139203b0a9528bee6b4b1cb5c7c9eba6b7
nvshink <nikinbox16@gmail.com>
2022-11-18 10:26:02 +0600

    Обновлены настройки Cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://vput.netlify.app"
 };
 
 app.use(cors(corsOptions));
